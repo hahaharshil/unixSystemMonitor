513f0819fdc66f6043f819232aa3af57aa76b706
Kritika Naagar <90194661+knaagar@users.noreply.github.com>
2026-05-04 10:18:53 +0530

    Update ui.c: Added colors

--- ui.c
+++ ui.c
@@ -6,24 +6,45 @@ void init_ui() {
     noecho();
     curs_set(0);
     nodelay(stdscr, TRUE);
+    
+    if (has_colors()) {
+        start_color();
+        init_pair(1, COLOR_GREEN, COLOR_BLACK); 
+        init_pair(2, COLOR_CYAN, COLOR_BLACK);
+        init_pair(3, COLOR_RED, COLOR_BLACK);
+    }
 }
 
 void draw_ui(Process *processes, int count, double cpu_usage, double mem_usage) {
     clear();
 
+    attron(COLOR_PAIR(1));
     mvprintw(0, 0, "CPU Usage: %.2f%%    MEM Usage: %.2f%%", cpu_usage, mem_usage);
-    mvprintw(1, 0, "--------------------------------------");
+    attroff(COLOR_PAIR(1));
+
+    mvprintw(1, 0, "-------------------------------------------");
+
+    attron(COLOR_PAIR(2));
     mvprintw(2, 0, "PID     NAME            CPU%%     MEM(B)");
+    attroff(COLOR_PAIR(2));
 
     int row = 3;
 
     for (int i = 0; i < 10 && i < count; i++) {
+        if (processes[i].cpu_percent > 7.0) {
+            attron(COLOR_PAIR(3));
+        }
+        
         mvprintw(row++, 0,
             "%-7d %-15s %-8.2f %-10ld",
             processes[i].pid,
             processes[i].name,
             processes[i].cpu_percent,
             processes[i].memory);
+            
+        if (processes[i].cpu_percent > 7.0) {
+            attroff(COLOR_PAIR(3));
+        }
     }
 
     refresh();
